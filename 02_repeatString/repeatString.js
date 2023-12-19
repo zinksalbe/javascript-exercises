dc381f739bbeaaff320da9f355ed07b53563fcb0
Zinksalbe <hoang-kh@hotmail.de>
2023-12-19 14:07:44 +0100

    Current ex. 04 ongoing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) return "ERROR";
+    let appendedString = "";
+    for (let i = 0; i < num; i++) {
+        appendedString += word;
+    }
+    return appendedString;
 };
 
 // Do not edit below this line
